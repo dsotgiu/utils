86cdd6847f5f61f29a2ec8a1a304ffe34e823f6b
dsotgiu <49731794+dsotgiu@users.noreply.github.com>
2024-02-21 16:11:13 +0100

    Update base64-xlsx.js
    added window.getSelection().toString()

--- base64-xlsx.js
+++ base64-xlsx.js
@@ -1,2 +1,4 @@
 /*converts a base64 value into a file excel*/
 const fn= (file64) => window.open('data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,' + file64);
+
+fn(window.getSelection().toString());
